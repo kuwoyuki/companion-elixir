3e635b6fe58d1fae6823f08ff29a19e6d2eb0f0b
kuwoyuki <kuwoyuki@cock.li>
2019-11-17 20:01:42 +0100

    zzz, wrong uid token

--- lib/companion/auth.ex
+++ lib/companion/auth.ex
@@ -44,7 +44,7 @@ hQIDAQAB
     body = %{
       appVersion: @app_version,
       platform: @platform.android,
-      uid: Config.get()[:user_id] |> rsa_encrypt()
+      uid: Config.get()[:uid]
     }
 
     %{"token" => token} = Companion.API.Base.request(:post, "login/token", body)
