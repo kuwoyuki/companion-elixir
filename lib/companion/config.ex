9a1c6b713cb14884b5f8f79229ccb02469a99005
kuwoyuki <kuwoyuki@cock.li>
2019-11-17 02:23:24 +0100

    process -> application

--- lib/companion/config.ex
+++ lib/companion/config.ex
@@ -1,14 +1,15 @@
 defmodule Companion.Config do
   @spec get :: any
-  def get, do: Process.get(:_companion_config, [])
+  def get, do: Application.get_env(:companion_ex, :config, [])
 
   @spec set(any) :: :ok
   def set(value) do
-    Process.put(:_companion_config, value)
+    Application.put_env(:companion_ex, :config, value)
     :ok
   end
 
   @spec append(any, any) :: any
-  def append(k, v), do: Process.put(:_companion_config, Keyword.put(get(), k, v)) # wat
+  # wat
+  def append(k, v), do: Application.put_env(:companion_ex, :config, Keyword.put(get(), k, v))
   :ok
 end
